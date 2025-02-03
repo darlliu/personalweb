f137b56d0ad40851be7d3cf3c1a58cc76b64a3a5
darlliu <darlliu@lavabit.com>
2025-02-03 13:39:12 -0800

    paran

--- src/clj/yuyu/blogs.clj
+++ src/clj/yuyu/blogs.clj
@@ -1,6 +1,6 @@
 (ns yuyu.blogs)
 (defn get-blog-posts []
-  ([{:title "Multi-Modal Model based on Distilled DeepSeek R1"
-     :date  "2025-02-05"
-     :fname "blog/multimodal.md"
-     :slug "multimodal"}])
+  [{:title "Multi-Modal Model based on Distilled DeepSeek R1"
+    :date  "2025-02-05"
+    :fname "blog/multimodal.md"
+    :slug "multimodal"}])
