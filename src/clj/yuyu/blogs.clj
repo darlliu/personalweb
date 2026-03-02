4dbd4b4987ebcfa5d14962a2669358ddbe8b580c
darlliu <darlliu@lavabit.com>
2026-03-02 03:07:32 -0800

    fix: Support loading blog markdown files from inside Uberjar archives

--- src/clj/yuyu/blogs.clj
+++ src/clj/yuyu/blogs.clj
@@ -15,16 +15,30 @@
         {:meta meta :body body})
       {:meta {} :body content})))
 
+(defn get-blog-files []
+  (let [url (io/resource "blog")]
+    (when url
+      (if (= "jar" (.getProtocol url))
+        (let [conn (.openConnection url)
+              jar-file (.getJarFile conn)
+              entries (enumeration-seq (.entries jar-file))]
+          (->> entries
+               (map #(.getName %))
+               (filter #(and (str/starts-with? % "blog/")
+                             (str/ends-with? % ".md")))
+               (map io/resource)))
+        (let [dir (io/file url)]
+          (when (.exists dir)
+            (filter #(and (.isFile %) (str/ends-with? (.getName %) ".md"))
+                    (file-seq dir))))))))
+
 (defn get-blog-posts []
-  (let [dir (io/file (.getFile (io/resource "blog")))
-        files (if (.exists dir)
-                (filter #(and (.isFile %) (str/ends-with? (.getName %) ".md")) (file-seq dir))
-                [])]
+  (if-let [files (get-blog-files)]
     (->> files
          (map (fn [f]
                 (let [content (slurp f)
-                      {:keys [meta body]} (parse-frontmatter content)
-                      fname (str "blog/" (.getName f))]
-                  (merge meta {:fname fname :body body}))))
+                      {:keys [meta body]} (parse-frontmatter content)]
+                  (merge meta {:body body}))))
          (sort-by :date)
-         reverse)))
+         reverse)
+    []))
